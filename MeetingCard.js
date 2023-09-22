41948d6c516ca9bb04ac5de7d8f4241fca9ee601
iamguddu1234 <iamguddu37@gmail.com>
2023-09-22 12:55:58 +0530

    update

--- MeetingCard.js
+++ MeetingCard.js
@@ -4,18 +4,24 @@ import { BsLink45Deg } from "react-icons/bs";
 function MeetingCard() {
   return (
     <div className={classes.parentsCard}>
-      <div className={classes.mainContainer}>
-        <div className={classes.meetingText}>Meeting Link :</div>
 
-        <button className={classes.copyLinkBtn}>
-          <BsLink45Deg />
-          <div className={classes.copyText}>Copy</div>
-        </button>
 
-        
+
+      <div className={classes.mainParentCard}>
+        <div className={classes.mainContainer}>
+          <div className={classes.meetingText}>Meeting Link :</div>
+
+          <button className={classes.copyLinkBtn}>
+            <BsLink45Deg />
+            <div className={classes.copyText}>Copy</div>
+          </button>
+        </div>
+
+        <button className={classes.JoinClassButton}>Join Class</button>
       </div>
 
-      <button className={classes.JoinClassButton}>Join Class</button>
+      <button className={classes.mettingLinkUrl}>https://app.diracai.com/video/meet.24.1695365080325.81.58574275809029
+      </button>
     </div>
   );
 }
